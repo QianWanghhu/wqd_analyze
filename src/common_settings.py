604a9c6eb18f30d1f4da3bc069b689d13e5da2cc
Qian Wang <qian@hhu.edu.cn>
2021-02-20 21:42:18 +0800

    Add the output of mod_load_flow

--- src/common_settings.py
+++ src/common_settings.py
@@ -4,16 +4,22 @@ from utils.concentration import rainfall_events
 
 # read the discrete storm events
 obspath = '../data/obs/'
-modpath = '../data/obs/'
+modpath = '../data/mod/'
 outpath = '../output/'
-events_name = 'obs_storm_event.csv'
+events_name = 'obs_storm_event_common.csv'
 obs_events = rainfall_events(obspath + events_name)
 
 # Read daily loads and flow
 day_load_flow = pd.read_csv(obspath+'low_interp_flow.csv', index_col='Date')
+day_load_flow.index = pd.to_datetime(day_load_flow.index)
 
 # Read hourly loads and flow
 fn_conct = 'cq-NO3'
 conct_name = fn_conct.split('-')[1]
 hour_load_flow = pd.read_csv(f'{obspath}high_{conct_name}_flow.csv', index_col = 'Time')
 hour_load_flow.index = pd.to_datetime(hour_load_flow.index)
+
+# Read modeling loads and flow
+mod_fl_fn = 'DIN_flow.csv'
+mod_load_flow = pd.read_csv(modpath + mod_fl_fn, index_col='Date')
+mod_load_flow.index = pd.to_datetime(mod_load_flow.index, dayfirst=True)
